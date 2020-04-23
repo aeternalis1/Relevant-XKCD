84ca264b2285ce89eed2f675968509e6fcf28c01
Michael Huang <michaelhuang88@outlook.com>
2020-04-22 23:34:46 -0400

    test

---
+++ rxkcd/instance/config.py
@@ -0,0 +1,2 @@
+SECRET_KEY = b'\xc8\xaaaZ\x922\x88\x19\xa0I\x8c\x81\xb0 H\xc1\xc7 zy\x16VvB'
+MONGO_URI = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox-qtnas.mongodb.net/test?retryWrites=true&w=majority"
